b5b2d242c232c6f3651fbf819d8caafbada3b91f
kaylane <kaylanesattilo2005@gmail.com>
2026-05-20 09:31:41 -0300

    função de adicionanr task

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function saveTask(){
     localStorage.setItem("task", JSON.stringify(task));
 }
 
+function addTask(){
+    const input = document.getElementById("input");
+
+    if (input = document.getElementById("input"));
+     
+    tasks.push(input.value);
+
+    saveTask();
+
+    creatTaskElement(input.value);
+
+    input.value = "";
+}
