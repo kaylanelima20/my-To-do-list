a0480213b40803b02d6c7f7d3c2c2717b5f6b387
kaylane <kaylanesattilo2005@gmail.com>
2026-05-20 09:21:29 -0300

    função salvar task

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-function addTask() {
-    const input = document.getElementById("input");
-    const list = document.getElementById("list");
+let task = [];
 
-    if (input.value ==="") return;
-
-    const li = document.createElement("li");
-    li.textContent = input.value;
-
-    // clicar para concluir tarefa
-    li.onclick = function () {
-        li.style.textDecoration = "line-through";
-    };
-
-    // remover
-    const btn = document.createElement("button");
-    btn.textContent = "X";
-
-    btn.onclick = function () {
-        list.removeChild(li);
-    };
-
-    li.appendChild(btn);
-    list.appendChild(li);
-
-    input.value = "";
+function saveTask(){
+    localStorage.setItem("task", JSON.stringify(task));
 }
+
