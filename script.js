893ef5e93040b610f35bf92028111b3cb787859b
kaylane <kaylanesattilo2005@gmail.com>
2026-05-20 09:51:44 -0300

    função createTaskElement

--- script.js
+++ script.js
@@ -13,7 +13,36 @@ function addTask(){
 
     saveTask();
 
-    creatTaskElement(input.value);
+    createTaskElement(input.value);
 
     input.value = "";
+}
+
+function createTaskElement(taskText) {
+
+    const list = document.getElementById("list");
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+    li.onlink = function () {
+        li.classList.toggle("done");
+    };
+
+    const btn = document.createElement("button");
+
+    btn.textContent = "X";
+
+    btn.onlink = function () {
+
+        list.removeChild(li);
+
+        task = tasks.filter(task => task !== taskText);
+
+        saveTask();
+    };
+
+    li.appendChild(btn);
+
+    list.appendChild(li);
 }
